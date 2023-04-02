d169e91df01fd4611c66533055bf7214a5951d04
BossyRomain <romain.bossy.01@gmail.com>
2023-04-02 17:22:13 +0200

    Ajout de l'algorithme principal.

--- src/grille.py
+++ src/grille.py
@@ -1,8 +1,9 @@
-# Classe représentant la grille
 import copy
 import random
+import cv2
 
 
+# Classe représentant la grille
 class Grille:
     def __init__(self, definition_cible, taille_cases, tailles_pochettes):
         self.taille_cases = taille_cases
@@ -20,9 +21,13 @@ class Grille:
     # que la longueur des côtés en nombre de cases d'un carré de cases libres sur la grille
     def trouver_carre_libre(self):
         toutes_tailles = [elem[0] for elem in self.tailles_pochettes]
-        taille = max(toutes_tailles)
+        if len(toutes_tailles) == 0:
+            taille = 1
+        else:
+            taille = max(toutes_tailles)
 
         trouver = False
+        coords = self.cases_libres[0]
         while taille > 1 and not trouver:
             i = 0
             while i < len(self.cases_libres) and not self.est_carrer_libre(self.cases_libres[i], taille):
@@ -43,7 +48,7 @@ class Grille:
             return False
 
         i = j = 0
-        while j < taille and (coords[0] + i, coords[1] + j) in self.cases_libres:
+        while j < taille and self.cases[coords[1] + j][coords[0] + i] is None:
             i += 1
             if i == taille:
                 i = 0
@@ -64,7 +69,8 @@ class Grille:
                 self.cases_libres.remove((x + i, y + j))
 
         if taille == 1:
-            self.cases[y][x] = copy.deepcopy(image.pixels)
+            img = cv2.resize(image.pixels, (taille * self.taille_cases, taille * self.taille_cases))
+            self.cases[y][x] = copy.deepcopy(img)
         else:
             index = 0
             while self.tailles_pochettes[index][0] != taille:
@@ -73,7 +79,21 @@ class Grille:
             if self.tailles_pochettes[index][1] == 0:
                 self.tailles_pochettes.pop(index)
 
-            step = int(100 / taille)
-            for i in range(taille):
-                for j in range(taille):
-                    self.cases[y + j][x + i] = image.pixels[i * step:(i + 1) * step, j * step:(j + 1) * step]
+            L = self.taille_cases * taille
+            step = int(L / taille)
+            img = cv2.resize(image.pixels, (L, L))
+            for j in range(taille):
+                for i in range(taille):
+                    x_start = i * step
+                    x_end = (i + 1) * step
+                    y_start = j * step
+                    y_end = (j + 1) * step
+                    self.cases[y + j][x + i] = img[y_start:y_end, x_start:x_end]
+
+    # Résultat : retourne l'image résultat
+    def obtenir_image_resultat(self):
+        lignes_fusionnees = []
+        for y in range(self.dimensions[1]):
+            lignes_fusionnees.append(cv2.hconcat(self.cases[y]))
+
+        return cv2.vconcat(lignes_fusionnees)
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from src.parametres import lire_parametres
 from src.erreurs import afficherErreurEtQuitter
 from src.images import recuperer_pochettes
 from src.grille import Grille
+import cv2
 
 
 def main():
@@ -13,11 +14,22 @@ def main():
 
     images = recuperer_pochettes(dossier_pochettes, poids_pochettes)
 
-    grille = Grille(definition_cible, taille_cases, tailles_pochettes)
+    key = -5
+    while key != ord('s'):
+        grille = Grille(definition_cible, taille_cases, tailles_pochettes)
+        i = 0
+        while len(grille.cases_libres) > 0:
+            coords, taille = grille.trouver_carre_libre()
 
-    coords, taille = grille.trouver_carre_libre()
+            grille.ajouter_image(images[i % len(images)], coords, taille)
+            i += 1
 
-    grille.ajouter_image(images[0], coords, taille)
+        image_resultat = grille.obtenir_image_resultat()
+        cv2.imshow("resultat", image_resultat)
+        key = cv2.waitKey(0)
+        del grille
+
+    cv2.imwrite(f"{dossier_sauvegarde}{nom}.{format}", image_resultat)
 
 
 if __name__ == "__main__":
